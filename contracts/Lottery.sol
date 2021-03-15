3b439661d1ef839d4e07a118cb3178867e912971
gregless22 <gregoryconnolly@gmail.com>
2021-03-15 15:37:55 +1100

    finsihed

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -8,28 +8,28 @@ contract Lottery {
         manager = msg.sender;
     }
 
-    // function enter() public payable {
-    //     require(msg.value > 0.01 ether);
-    //     participants.push(msg.sender);
-    // }
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function enter() public payable {
+        require(msg.value > 0.01 ether);
+        participants.push(msg.sender);
+    }
 
     // // Not really random
-    // function random() private view returns (uint256) {
-    //     return uint256(sha3(block.difficulty, now, participants));
-    // }
+    function random() private view returns (uint256) {
+        return uint256(sha3(block.difficulty, now, participants));
+    }
 
-    // function pickWinner() public restricted {
-    //     uint256 index = random % participants.length;
-    //     participants[index].transfer(this.balance);
-    //     participants = new address[](0);
-    // }
+    function pickWinner() public restricted {
+        uint256 index = random() % participants.length;
+        participants[index].transfer(this.balance);
+        participants = new address[](0);
+    }
 
-    // function returnParticipants() public view returns (address[]) {
-    //     return participants;
-    // }
-
-    // modifier restricted() {
-    //     require(msg.sender == manager);
-    //     _;
-    // }
+    function getParticipants() public view returns (address[]) {
+        return participants;
+    }
 }
--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -8,24 +8,110 @@ const web3 = new Web3(provider)
 
 let accounts
 let lottery
-const INITIAL_MESSAGE = "Hello World!"
-const CHANGED_MESSAGE = "New Message"
+let MANAGER
+let PLAYER1
+let PLAYER2
 
 beforeEach(async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts()
+  MANAGER = accounts[0]
+  PLAYER1 = accounts[1]
+  PLAYER2 = accounts[2]
   // use one of the accounts to deploy contract
-  lottery = await new web3.eth.Lottery(JSON.parse(interface))
+  lottery = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
-      data: bytecode,
+      data: "0x" + bytecode,
     })
-    .send({ from: accounts[0], gas: "1000000" })
+    .send({ from: MANAGER, gas: "1000000" })
 
-  inbox.setProvider(provider)
+  lottery.setProvider(provider)
 })
 
 describe("Lottery", () => {
   it("deploys a new lottery", async () => {
     assert.ok(lottery.options.address)
+  })
+  it("allows one account to enter a lottery", async () => {
+    await lottery.methods.enter().send({
+      from: PLAYER1,
+      gas: "1000000",
+      value: web3.utils.toWei("0.02", "ether"),
+    })
+    const participants = await lottery.methods
+      .getParticipants()
+      .call({ from: MANAGER })
+
+    assert.equal(PLAYER1, participants[0])
+    assert.equal(participants.length, 1)
+  })
+  it("allows multiple accounts to enter", async () => {
+    await lottery.methods.enter().send({
+      from: PLAYER1,
+      gas: "1000000",
+      value: web3.utils.toWei("0.02", "ether"),
+    }),
+      await lottery.methods.enter().send({
+        from: PLAYER2,
+        gas: "1000000",
+        value: web3.utils.toWei("0.02", "ether"),
+      })
+    const participants = await lottery.methods
+      .getParticipants()
+      .call({ from: MANAGER })
+    assert.equal(participants.length, 2)
+  })
+  it("dont allow to enter without enough ether", async () => {
+    try {
+      await lottery.methods.enter().send({
+        from: PLAYER1,
+        gas: "1000000",
+        value: web3.utils.toWei("0.002", "ether"),
+      })
+      assert(false)
+    } catch (err) {
+      assert(err)
+    }
+
+    const participants = await lottery.methods
+      .getParticipants()
+      .call({ from: MANAGER })
+    assert.equal(participants.length, 0)
+  })
+
+  it("pick winner is not manager", async () => {
+    try {
+      await lottery.methods.pickWinner().send({
+        from: PLAYER1,
+        gas: "1000000",
+      })
+      assert(false)
+    } catch (err) {
+      assert(err)
+    }
+  })
+
+  it("pick winner is a manager", async () => {
+    await lottery.methods.enter().send({
+      from: PLAYER2,
+      gas: "1000000",
+      value: web3.utils.toWei("2", "ether"),
+    })
+
+    const intialBalance = await web3.eth.getBalance(PLAYER2)
+
+    await lottery.methods.pickWinner().send({
+      from: MANAGER,
+      gas: "1000000",
+    })
+
+    const finalBalance = await web3.eth.getBalance(PLAYER2)
+    const difference = finalBalance - intialBalance
+    assert(difference > web3.utils.toWei("1.8", "ether")) // 1.8 allows for some gas loss
+
+    const participants = await lottery.methods
+      .getParticipants()
+      .call({ from: MANAGER })
+    assert.equal(participants.length, 0)
   })
 })
